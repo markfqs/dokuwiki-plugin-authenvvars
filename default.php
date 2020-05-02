c99a018d8da916b0d13c0e6c7101719c5f444d19
Christian Hoffmann <47904800+chrille69@users.noreply.github.com>
2020-05-03 01:22:45 +0200

    für conf

---
+++ default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Default settings for the authenvvars plugin
+ *
+ * @author Christian Hoffmann <christian@lehrer-hoffmann.de>
+ */
+
+$conf['useridvar']   = 'REMOTE_USER';
+$conf['usernamevar'] = 'REMOTE_USERNAME';
+$conf['emailvar']    = 'REMOTE_EMAIL';
+$conf['groupsvar']   = 'REMOTE_GROUPS';
+$conf['groupattr']   = '';
