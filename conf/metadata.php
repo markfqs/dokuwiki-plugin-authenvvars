d640075ac84b2b0b1c731ad4d19df42238ed28a7
Christian Hoffmann <47904800+chrille69@users.noreply.github.com>
2020-05-03 01:26:35 +0200

    Import

---
+++ conf/metadata.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Options for the authenvvars plugin
+ *
+ * @author Christian Hoffmann <christian@lehrer-hoffmann.de>
+ */
+
+$meta['useridvar']   = array('string', '_cautionList' => array('plugin____authhttp____userregex' => 'danger'));
+$meta['usernamevar'] = array('string');
+$meta['emailvar']    = array('string');
+$meta['groupsvar']   = array('string');
+$meta['groupattr']   = array('string');
